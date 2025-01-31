0e9eef641a579c47a1fb18cb7a077d2f433fefdb
benielcapo <169079689+benielcapo@users.noreply.github.com>
2025-01-31 13:05:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var HOLD = document.getElementById("holder")
+var COOKIES = document.cookie
+if (COOKIES == "") {
+    HOLD.innerHTML = "no cookies, saving your data!"
+    document.cookie = "hello, world!"
+} else {
+    HOLD.innerHTML = document.cookie
+}
