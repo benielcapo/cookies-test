b1f4bccef4d847b0a1a27c58b9fdb39cacef3f8e
benielcapo <169079689+benielcapo@users.noreply.github.com>
2025-01-31 14:28:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var HOLD = document.getElementById("holder")
 var COOKIES = document.cookie
-console.log(COOKiES)
+console.log(COOKIES)
 if (COOKIES == "") {
     HOLD.innerHTML = "no cookies, saving your data!"
     document.cookie = "hello=world; expires=Thu, 31 Dec 2025 12:00:00 UTC!"
