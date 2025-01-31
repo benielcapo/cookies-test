2ec2c1c26329ff07e57a960abb3db1414ab0a241
benielcapo <169079689+benielcapo@users.noreply.github.com>
2025-01-31 13:12:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var HOLD = document.getElementById("holder")
-var COOKIES = document.cookie
+var COOKIES = document.cookie || ""
 if (COOKIES == "") {
     HOLD.innerHTML = "no cookies, saving your data!"
     document.cookie = "hello, world!"
 } else {
-    HOLD.innerHTML = document.cookie
+    HOLD.innerHTML = COOKIES
 }
