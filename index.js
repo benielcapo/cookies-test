bb524d7908f7c2dae3c5fb7e939ac7c6b449ac1a
benielcapo <169079689+benielcapo@users.noreply.github.com>
2025-01-31 14:18:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var HOLD = document.getElementById("holder")
 var COOKIES = document.cookie
+console.log(COOKiES)
 if (COOKIES == "") {
     HOLD.innerHTML = "no cookies, saving your data!"
     document.cookie = "hello, world!"
